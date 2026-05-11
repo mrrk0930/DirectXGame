825709140be29fa53e83cfe9daa0352ff576140f
gc2b_murosaki <k025g1008@g.neec.ac.jp>
2026-05-11 16:19:02 +0900

    プレイヤーヘッダーの変更

--- Player.h
+++ Player.h
@@ -1,16 +1,15 @@
 #pragma once
 #include "KamataEngine.h"
 
-//プレイヤー
-class Player 
-{
+// プレイヤー
+class Player {
 
-	public:
+public:
 	// 初期化
-		///<param name="model">モデル</param>
-		///<param name="textureHandle">テクスチャハンドル</param>
-		///<param name="camera">カメラ</param>
-	    void Initialize(KamataEngine::Model* model, uint32_t textureHandle, KamataEngine::Camera* camera);
+	///< param name="model">モデル</param>
+	///< param name="textureHandle">テクスチャハンドル</param>
+	///< param name="camera">カメラ</param>
+	void Initialize(KamataEngine::Model* model, uint32_t textureHandle, KamataEngine::Camera* camera);
 
 	// 更新
 	void Update();
@@ -18,17 +17,16 @@ class Player
 	// 描画
 	void Draw();
 
-	private:
-        //ワールド変換データ
-	    KamataEngine::WorldTransform worldTransform_;
+private:
+	// ワールド変換データ
+	KamataEngine::WorldTransform worldTransform_;
 
-		//モデル
-	    KamataEngine::Model* model_ = nullptr;
+	// モデル
+	KamataEngine::Model* model_ = nullptr;
 
-		//テクスチャハンドル
-	    uint32_t textureHandle_ = 0u;
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
 
-		//カメラ
-	    KamataEngine::Camera* camera_ = nullptr;
-
+	// カメラ
+	KamataEngine::Camera* camera_ = nullptr;
 };
