999ac5638185738b869f40d1049c3df2522dcbe5
gc2b_murosaki <k025g1008@g.neec.ac.jp>
2026-05-11 16:20:53 +0900

    プレイヤーcpp変更

--- Player.cpp
+++ Player.cpp
@@ -3,39 +3,32 @@
 
 using namespace KamataEngine;
 
-void Player::Initialize(KamataEngine::Model* model, uint32_t textureHandle, KamataEngine::Camera* camera) 
-{
+void Player::Initialize(KamataEngine::Model* model, uint32_t textureHandle, KamataEngine::Camera* camera) {
 
 	// インゲームの初期化処理
 
-	//NULLポインタチャック
+	// NULLポインタチャック
 	assert(model);
-	
-	//引数として受け取ったデータをメンバ変数に記録する
+
+	// 引数として受け取ったデータをメンバ変数に記録する
 	model_ = model;
 	textureHandle_ = textureHandle;
 	camera_ = camera;
 
-	//ワールド変換の初期化
+	// ワールド変換の初期化
 	worldTransform_.Initialize();
 }
 
-void Player::Update() 
-{
+void Player::Update() {
 
 	// インゲームの更新処理
 
-	//行列を定数バッファに転送
+	// 行列を定数バッファに転送
 	worldTransform_.TransferMatrix();
-
-
 }
 
-void Player::Draw() 
-{
+void Player::Draw() {
 
-	//3Dモデルを描画
+	// 3Dモデルを描画
 	model_->Draw(worldTransform_, *camera_, textureHandle_);
-
 }
-
